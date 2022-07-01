86cc7f9c16e64beaf84ab448ac219a446b6ec633
kamalCodEnterprise <97885287+kamalCodEnterprise@users.noreply.github.com>
2022-07-01 11:09:05 +0500

    updated state

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -13,9 +13,23 @@ const cartSlice = createSlice({
     reducers: {
         add(state, action){
             
-            state.push(action.payload)
-   
-           state.map((item)=> (item.id===action.payload.id ? `  ${item.id++} ` : item   ))
+           const index = state.findIndex(item => item.id === action.payload.id);
+
+           if (index === -1) {
+             state.push({
+               ...action.payload,
+               quantity: 1
+             });
+             const updateCart = [...state];
+             console.log("carffft" + updateCart);
+           } else {
+            state[index].quantity += 1;
+             const updateCart = [...state];
+             console.log("updtaed cart" + updateCart);
+             
+           }
+           
+            
            
            
             
